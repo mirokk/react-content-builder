35060bad2e6a302dc3cbcebb612e64e253eca8e3
Miro K <cosmixes@gmail.com>
2016-08-21 04:00:05 -0600

    bugfix reinitialising of dragula

--- src-dev/Server.js
+++ src-dev/Server.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
 });
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
